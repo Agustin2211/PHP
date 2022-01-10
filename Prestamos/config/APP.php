3eba8393ce5569e454b7958056319845d67a08fd
Agustin2211 <72230884+Agustin2211@users.noreply.github.com>
2022-01-09 21:47:27 -0300

    Archivos Config Terminados

---
+++ Prestamos/config/APP.php
@@ -0,0 +1,10 @@
+<?php
+
+    const SERVERURL="http://localhost/Prestamos/";
+
+    const COMPANY = "SISTEMA DE PRESTAMOS";
+
+    const MONEDA = "$";
+
+    date_default_timezone_set("America/Argentina/Buenos_Aires");
+?>
